0d4672f8ca3511cbf44ad10a77a1cab16aef1f36
Zain Naveed <zainnaveed359@gmail.com>
2026-04-01 00:54:50 +0500

    Refactor index.js to improve structure and readability

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const cookieOptions = {
 };
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173","https://chatconnecct.netlify.app"],
     credentials: true
 }));
 
